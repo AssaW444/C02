68fdba8bc153f1e6e9869cd35fed84df282658d6
Anthony <you@example.com>
2025-10-16 17:00:44 +0200

    atoi

--- ex02/ipssi_atoi.c
+++ ex02/ipssi_atoi.c
@@ -1,13 +1,15 @@
+#include <stdio.h>
+
 int	ipssi_atoi(char *str)
 {
 	int	i;
 	int	nbr_moins;
 	int	signe;
-	int	conv;
-
+	int 	result;
+	
 	i = 0;
 	nbr_moins = 0;
-	conv = 0;
+	result = 0;
 	while (str[i])
 	{
 		if (str[i] == ' ' || str[i] == '\n' || str[i] == '\r'
@@ -19,13 +21,19 @@ int	ipssi_atoi(char *str)
 		{	nbr_moins += 1;
 			i++;
 		}
-		conv = conv *10 + (str[i] + '0');
-		return (conv);
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			result = result * 10 + (str[i] - '0');
+			i++;
+		}
 	}
 
 	if (nbr_moins % 2 != 0)
 		signe = -1;
+	return (result * signe);
+}
+int main()
+{
+        printf("%d\n", ipssi_atoi("+-+--48954366"));
 
-	return (signe);
-        i++;	
-}	
+}
